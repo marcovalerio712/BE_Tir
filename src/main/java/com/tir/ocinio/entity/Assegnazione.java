06748e720391a6b4bde26f745baed36a196f3be7
VeronicaLazaro <Veronicalazaro30@gmail.com>
2023-09-29 10:38:51 +0200

    Creata pojo assegnazione

---
+++ src/main/java/com/tir/ocinio/entity/Assegnazione.java
@@ -0,0 +1,45 @@
+package com.tir.ocinio.entity;
+
+public class Assegnazione {
+	
+	private Dipendente dipendente;
+	
+	private Commessa commessa;
+	
+	private Long competenza;
+	
+	private Long attivo;
+
+	public Dipendente getDipendente() {
+		return dipendente;
+	}
+
+	public void setDipendente(Dipendente dipendente) {
+		this.dipendente = dipendente;
+	}
+
+	public Commessa getCommessa() {
+		return commessa;
+	}
+
+	public void setCommessa(Commessa commessa) {
+		this.commessa = commessa;
+	}
+
+	public Long getCompetenza() {
+		return competenza;
+	}
+
+	public void setCompetenza(Long competenza) {
+		this.competenza = competenza;
+	}
+
+	public Long getAttivo() {
+		return attivo;
+	}
+
+	public void setAttivo(Long attivo) {
+		this.attivo = attivo;
+	}
+	
+}
