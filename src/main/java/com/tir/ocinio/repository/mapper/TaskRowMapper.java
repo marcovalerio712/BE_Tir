180d57287a987982096c5eea0cc42ebf9db1097e
LordBalduin <a.marchionne@outlook.it>
2023-10-02 15:15:24 +0200

    TaskRowMapper incompleto
    Da finire

---
+++ src/main/java/com/tir/ocinio/repository/mapper/TaskRowMapper.java
@@ -0,0 +1,31 @@
+package com.tir.ocinio.repository.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.tir.ocinio.entity.Task;
+
+public class TaskRowMapper implements RowMapper<Task>{
+	
+	//private CommessaRowMapper commessaMapper = new CommessaRowMapper();
+	private DipendenteRowMapper dipendenteMapper = new DipendenteRowMapper();
+
+	@Override
+	public Task mapRow(ResultSet rs, int rowNum) throws SQLException {
+		
+		var task = new Task();
+		
+		
+		task.setId(rs.getLong("tas_id"));
+		task.setDescrizione(rs.getString("tas_descrizione"));
+		task.setNome(rs.getString("tas_nome"));
+		task.setData_assegnazione(rs.getDate("tas_data_assegnazione"));
+		task.setData_consegna(rs.getDate("tas_data_consegna"));
+		//task.setCommessa();
+		task.setDipendente(null);
+		return task;
+	}
+
+}
