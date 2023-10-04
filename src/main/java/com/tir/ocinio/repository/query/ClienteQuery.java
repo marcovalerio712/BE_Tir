54c6fee9a93385f9b8014c49e737c86c48659ca8
LordBalduin <a.marchionne@outlook.it>
2023-10-04 11:38:42 +0200

    ClienteQuery final hotfix

--- src/main/java/com/tir/ocinio/repository/query/ClienteQuery.java
+++ src/main/java/com/tir/ocinio/repository/query/ClienteQuery.java
@@ -5,7 +5,7 @@ public class ClienteQuery {
 	public static String allClienti = "select c.id cli_id, c.nome cli_nome, c.p_iva cli_piva, c.telefono cli_telefono " 
 									+ " from clienti c";
 	
-	public static String oneCliente = allClienti + "where c.id = %d";
+	public static String oneCliente = allClienti + " where c.id = %d";
 	
 	public static String countClienti = "select count(*) from clienti";	
 
