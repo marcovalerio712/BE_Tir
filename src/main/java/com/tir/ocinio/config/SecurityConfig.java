d81371a349be23f639789ed538a390d0e140f0cf
lorenzodigio <lorenzodigiovanni2000@gmail.com>
2023-10-06 10:17:31 +0200

    encoder changes

--- src/main/java/com/tir/ocinio/config/SecurityConfig.java
+++ src/main/java/com/tir/ocinio/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+/*import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;*/
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,11 +14,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 	
 	@Bean
-	public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
-		return http
+	 SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+		return http				
 				.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
 				.formLogin(Customizer.withDefaults())
 				.build();
 	}
+	/*
+	 * Modo di criptare le password in java
+	 */
+	/*
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}*/
 
 }
